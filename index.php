27f4a177ac1a7a489ddf421f0ef49640de652356
EC2 Default User <ec2-user@ip-172-31-65-50.ec2.internal>
2020-10-24 23:23:35 +0000

    adding index file

--- index.php
+++ index.php
@@ -1,11 +1,38 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title> Lab 1: Personal Page</title>
+        <meta charset="utf-8">
     </head>
     <body>
         
-        <h1>Welcome to my website!</h1>
+        <header>
+            <h1>Sean Wilson</h1>
+        </header>
+        <hr>
+        
+        <nav>
+            <a href="index.html"> Home </a>
+            <a href="about.html"> About </a>
+            <a href="contact.html"> Contact Me </a>
+        </nav>
+        
+        <main>
+            
+            <figure>
+                <img src="img/SeanPic.jpg" width="100">
+            </figure>
+            
+            <div>
+                Welcome to my page!!!!
+            </div>
+            
+        </main>
+        
+        <footer>
+            
+        </footer>
+        
 
     </body>
 </html>
